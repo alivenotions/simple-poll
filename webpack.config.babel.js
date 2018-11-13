ac6708d7d4829fddc8ffeac049e06a21152ddce9
Bhavdeep Dhanjal <dhanjalbhavdeep@gmail.com>
2018-11-14 02:40:56 +0530

    fix: create umd builds
    umd builds to be consumed by browsers. This commit along with #3f3cc7 provide support for older
    versions of node and for browsers.
    
    fix #1

---
+++ webpack.config.babel.js
@@ -0,0 +1,23 @@
+import { join } from 'path'
+
+const include = join(__dirname, 'src')
+
+export default {
+  entry: './src/poll',
+  output: {
+    path: join(__dirname, 'dist'),
+    libraryTarget: 'umd',
+    library: 'simplePoll',
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+        }
+      }
+    ]
+  }
+}
